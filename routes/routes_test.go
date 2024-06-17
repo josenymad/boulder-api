1cc353c7ca9e7493b7832ea5b6a06486b58d1c00
Jose Wills <jose.wills@insurancerevolution.co.uk>
2024-06-17 11:47:20 +0100

    test create round

--- routes/routes_test.go
+++ routes/routes_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/josenymad/boulder-api/config"
@@ -20,6 +21,7 @@ func setUpRouter() *gin.Engine {
 	router := gin.Default()
 	router.POST("/competition", routes.CreateCompetition)
 	router.POST("/categories", routes.CreateCompetitionCategory)
+	router.POST("rounds", routes.CreateRound)
 	return router
 }
 
@@ -87,4 +89,42 @@ func TestCreateCompetitionCategory(t *testing.T) {
 		t.Fatalf("Failed to unmarshal JSON: %v", err)
 	}
 	assert.Equal(t, "Test Category", response["name"])
+}
+
+func TestCreateRound(t *testing.T) {
+	router := setUpRouter()
+	err := config.ConnectDB("test")
+	if err != nil {
+		log.Fatalf("Could not set up database: %v", err)
+	}
+
+	round := types.Round{
+		Number:        1,
+		StartDate:     time.Date(2024, time.July, 1, 10, 0, 0, 0, time.UTC),
+		EndDate:       time.Date(2024, time.July, 15, 19, 0, 0, 0, time.UTC),
+		CompetitionID: 15,
+	}
+	body, err := json.Marshal(round)
+	if err != nil {
+		t.Fatalf("Failed to marshal JSON: %v", err)
+	}
+
+	req, err := http.NewRequest("POST", "/rounds", bytes.NewBuffer(body))
+	if err != nil {
+		t.Fatalf("Failed to create request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 201, w.Code)
+	var response map[string]interface{}
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf("Failed to unmarshal JSON: %v", err)
+	}
+	assert.Equal(t, float64(1), response["number"])
+	assert.Equal(t, "2024-07-01T10:00:00Z", response["start_date"])
+	assert.Equal(t, "2024-07-15T19:00:00Z", response["end_date"])
+	assert.Equal(t, float64(15), response["competition_id"])
 }
